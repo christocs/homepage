309f9f2f88d37b28fbe0d8713494a61cc0900475
Christo Stephenson <christo@iinet.net.au>
2020-06-24 23:57:53 +0800

    fix particle position, make hello world text bigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import {Button} from "semantic-ui-react";
 import Particles from "react-particles-js";
 
-function App() {
-    return (
-        <React.Fragment>
-        <div className="particles">
-            <h1 style={{color: "cyan", textAlign: "center", verticalAlign: "center"}}>Hello world!</h1>
-        </div>
+export class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Particles className={"particles"}/>
 
-        <Particles canvasClassName={"particles"}/>
-        </React.Fragment>
-    );
+                <div className={"particlesContainer"} style={{display: "block"}}>
+                <h1 style={{color: "cyan", textAlign: "center", verticalAlign: "bottom", fontSize: "20rem"}}>Hello world!</h1>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
