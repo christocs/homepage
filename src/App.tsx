c765c4f1cf8f8107b18e36dd56495e3c438ab2ce
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2020-06-24 21:37:09 +0800

    Add hello world page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "semantic-ui-react";
+import Particles from "react-particles-js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+        <div className="particles">
+            <h1 style={{color: "cyan", textAlign: "center", verticalAlign: "center"}}>Hello world!</h1>
+        </div>
+
+        <Particles canvasClassName={"particles"}/>
+        </React.Fragment>
+    );
 }
 
 export default App;
