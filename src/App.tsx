baf384cb77719d0ed1a445602d56903bae126980
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2020-06-30 23:47:03 +0800

    Make website mobile-friendly

--- src/App.tsx
+++ src/App.tsx
@@ -26,47 +26,47 @@ export class App extends React.Component {
                 }}/>
 
                 <div className={"backdrop"}>
-                    <div style={{padding: "1rem"}}>
-                        <h1 className={"title"} style={{fontSize: "10rem"}}>Christo Stephenson</h1>
-                        <h2 className={"title"} style={{fontSize: "6rem"}}>Software Engineer</h2>
+                    <div>
+                        <h1>Christo Stephenson</h1>
+                        <h2>Software Engineer</h2>
                     </div>
 
                     <br/>
                     <br/>
                     <br/>
 
-                    <div style={{padding: "1rem"}}>
-                        <h2 className={"heading"} style={{fontSize: "4rem"}}>About Me</h2>
+                    <div>
+                        <h3>About Me</h3>
+                        <p>
+                            I am a software developer with a passion in creating software in
+                            a variety of fields, ranging from being a full-stack web developer to an embedded
+                            systems engineer. Currently I live and work in my hometown of Perth, Australia.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3>Skills</h3>
                         <div>
-                            <p className={"paragraph"}>I am a software developer with a passion in creating software in
-                                a variety of fields, ranging from being a full-stack web developer to an embedded
-                                systems engineer. Currently I live and work in my hometown of Perth, Australia.</p>
+                            <li>C</li>
+                            <li>C++</li>
+                            <li>C#</li>
+                            <li>Git</li>
+                            <li>Linux/Unix</li>
+                            <li>Java</li>
+                            <li>JavaScript</li>
+                            <li>MySQL/MariaDB</li>
+                            <li>PHP</li>
+                            <li>Python</li>
+                            <li>ReactJS</li>
                         </div>
                     </div>
 
-                    <div style={{padding: "1rem"}}>
-                        <h2 className={"heading"} style={{fontSize: "4rem"}}>Skills</h2>
+                    <div>
+                        <h3>Where to find me</h3>
                         <div>
-                            <li className={"list-item"}>C</li>
-                            <li className={"list-item"}>C++</li>
-                            <li className={"list-item"}>C#</li>
-                            <li className={"list-item"}>Git</li>
-                            <li className={"list-item"}>Linux/Unix</li>
-                            <li className={"list-item"}>Java</li>
-                            <li className={"list-item"}>JavaScript</li>
-                            <li className={"list-item"}>MySQL/MariaDB</li>
-                            <li className={"list-item"}>PHP</li>
-                            <li className={"list-item"}>Python</li>
-                            <li className={"list-item"}>ReactJS</li>
-                        </div>
-                    </div>
-
-                    <div style={{padding: "1rem"}}>
-                        <h2 className={"heading"} style={{fontSize: "4rem"}}>Where to find me</h2>
-                        <div>
-                            <a className={"list-item"} href={"https://www.linkedin.com/in/christo-stephenson/"}
+                            <a href={"https://www.linkedin.com/in/christo-stephenson/"}
                                target={"_blank"} rel={"noopener noreferrer"}>LinkedIn</a>
-                            <a className={"list-item"} href={"https://github.com/christocs/"}
+                            <a href={"https://github.com/christocs/"}
                                target={"_blank"} rel={"noopener noreferrer"}>GitHub</a>
                         </div>
                     </div>
