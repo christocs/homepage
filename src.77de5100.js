84eda7ad553e9afa23c09fc9066e1c417e8a1a5d
Christo Stephenson <46086756+christocs@users.noreply.github.com>
2021-04-22 21:45:16 +0800

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -11526,7 +11526,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64633" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56091" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
